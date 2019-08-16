4dc2f22cfd21bc34966983afa562e46a8c4c87f0
byron <byron@ganguo.hk>
2019-08-16 16:58:24 +0800

    feat: add basic code in py

---
+++ python/demo_test/basic.py
@@ -0,0 +1,17 @@
+def f(a, L=[]):
+    L.append(a)
+    return L
+
+def f1(a, L=None):
+    if L is None:
+        L = []
+    L.append(a)
+    return L
+
+def f2():
+    words = ['monkey', 'cat', 'dog', 'birds']
+    for word in words:
+        print(word)
+
+
+f2()
