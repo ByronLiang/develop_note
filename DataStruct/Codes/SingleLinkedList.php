c2f63b8942e67db4a203703aca051d64bf8d0774
byron <liangzhaocong@163.com>
2020-05-07 11:26:21 +0800

    feat: 单链数据增加闭环处理与检测单链闭环

--- DataStruct/Codes/SingleLinkedList.php
+++ DataStruct/Codes/SingleLinkedList.php
@@ -75,16 +75,62 @@ class SingleLinkedList
         }
         echo "last node: ". $node->data ."\n";
     }
+
+    /**
+     * 进行闭环处理
+     */
+    public function addCirclePoint(Node $node, $circlePointData)
+    {
+        if ($this->header == null && $this->last == null) {
+            return false;
+        }
+        $this->last->next = $node;
+        $this->last = $node;
+        $this->size +=1;
+        // 开始对指定节点进行闭环处理
+        $node = $this->header;
+        if ($node->data == $circlePointData) {
+            $this->last->next = $node;
+            return true;
+        }
+        while($node->next != null) {
+            $target = $node->next;
+            if ($target->data == $circlePointData) {
+                $this->last->next = $target;
+                return true;
+            }
+            $node = $node->next;
+        }
+    }
+
+    /**
+     * 判断是否闭环
+     */
+    public function judgeCycle()
+    {
+        $fast_node = $slow_node = $this->header;
+        while ($fast_node != null && $fast_node->next != null) {
+            $fast_node = $fast_node->next->next;
+            $slow_node = $slow_node->next;
+            if ($fast_node->data == $slow_node->data) return 1;
+        }
+
+        return 0;
+    }
 }
 
 $singleLinkedList = new SingleLinkedList();
 
-$nodes = [2, 5, 8];
+$nodes = [2, 5, 8, 10, 15];
 foreach ($nodes as $value) {
     $node = new Node($value);
     $singleLinkedList->add($node);
 }
+// 闭环操作
+// $singleLinkedList->addCirclePoint(new Node(10), 5);
+// $res = $singleLinkedList->judgeCycle();
+// echo $res."\n";
 
-$singleLinkedList->getAll();
-$singleLinkedList->reverse();
-$singleLinkedList->getAll();
+// $singleLinkedList->getAll();
+// $singleLinkedList->reverse();
+// $singleLinkedList->getAll();
