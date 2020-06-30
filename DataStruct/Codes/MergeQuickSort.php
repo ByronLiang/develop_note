2f8c0d32dc872aa88605e046d42ebff6d4011421
byron <liangzhaocong@163.com>
2020-06-30 17:59:04 +0800

    feat: upgrade some sort and find method

--- DataStruct/Codes/MergeQuickSort.php
+++ DataStruct/Codes/MergeQuickSort.php
@@ -2,8 +2,16 @@
 
 class SortHelper
 {
-    function MergeQuickSort($arr) {
-
+    /**
+     * 已排序数组
+     */
+    public $sortedArray;
+    
+    /**
+     * 快速排序
+     */
+    function MergeQuickSort($arr)
+    {
         if (empty($arr)) {
             return [];
         }
@@ -34,9 +42,60 @@ class SortHelper
         echo (PHP_EOL."end".PHP_EOL);
         return array_merge($left, [$provit], $right);
     }
+
+    /**
+     * 冒泡排序
+     */
+    public function bindSort(array &$arr)
+    {
+        for ($i = 0; $i < count($arr) - 1; $i++) {
+            for ($j = 0; $j < count($arr) - 1 - $i; $j++) {
+                if ($arr[$j] > $arr[$j + 1]) {
+                    list($arr[$j], $arr[$j+1]) = [$arr[$j + 1], $arr[$j]];
+                }
+            }
+        }
+
+        return $arr;
+    }
+
+    /**
+     * 二分法查找
+     */
+    public function find(int $target)
+    {
+        return $this->repFind($target, 0, count($this->sortedArray) - 1);
+    }
+
+    /**
+     * 递归处理二分法
+     */
+    protected function repFind($target, $left, $right)
+    {
+        if ($left > $right) {
+            return false;
+        }
+        $mid = $left + (int) (($right - $left) / 2);
+        // $mid = (int) (($right + $left) / 2);
+        if ($this->sortedArray[$mid] === $target) {
+            return $mid;
+        }
+        if ($this->sortedArray[$mid] > $target) {
+            // 向左缩小范围
+            return $this->repFind($target, $left, $mid - 1);
+        } else {
+            // 向右缩小范围
+            return $this->repFind($target, $mid + 1, $right);
+        }
+    }
 }
 
 $arr = array(2, 1, 5, 8, 3, 7);
 $obj = new SortHelper();
-$data = $obj->MergeQuickSort($arr);
-var_dump($data);
+// $data = $obj->MergeQuickSort($arr);
+
+$data = $obj->bindSort($arr);
+$obj->sortedArray = $data;
+$res = $obj->find(7);
+print_r($data);
+print_r($res);
