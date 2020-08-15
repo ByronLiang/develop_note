76544add4026db76210831527e9af458dc1d30b8
byron <liangzhaocong@163.com>
2020-08-15 15:38:26 +0800

    feat: memory cache
    1. go sample code about decorator

--- go/code/basic/MyTool/main.go
+++ go/code/basic/MyTool/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "test/tools"
+import (
+    "fmt"
+    "log"
+    "test/tools"
+)
 
 func main()  {
-    tools.CasualTimeCount(10)
+    //tools.CasualTimeCount(10)
+    DecoratorSample()
+}
+
+func DecoratorSample()  {
+    //type MyFoo func(int, int, int) int
+    origin := foo
+    // 装饰器模式 对传参按照修饰方法进行处理
+    _ = tools.Decorator(&origin, douFoo)
+    ews := origin(1, 2, 3)
+    log.Print(ews)
+}
+
+func douFoo(a, b, c int) int {
+    fmt.Printf("%d, %d, %d \n", a, b, c)
+    return a*2 + b*1 + c*3
+}
+
+func foo(a, b, c int) int {
+    fmt.Printf("%d, %d, %d \n", a, b, c)
+    return a + b + c
 }
