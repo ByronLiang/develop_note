11eaf8bb42d21c0b5bdfb75d388338e7798c2963
byron <liangzhaocong@163.com>
2020-09-01 12:01:09 +0800

    feat: add datastruct code
    1. note about golang lock

--- DataStruct/Codes/Dfs.php
+++ DataStruct/Codes/Dfs.php
@@ -8,7 +8,7 @@ class dfs {
         $this->data = json_decode(file_get_contents('./data/sample.json'), true);
     }
 
-    public function DfsMap()
+    public function dfsMap()
     {
         $stack = [];
         $list = [];
@@ -28,6 +28,46 @@ class dfs {
     }
 }
 
-$obj = new dfs();
-$res = $obj->DfsMap();
+class Solution {
+
+    protected $result = [];
+
+    /**
+     * https://leetcode-cn.com/problems/permutations
+     * 
+     * 全排列: 回溯-递归
+     */
+    function permute($nums) {
+        $count = count($nums);
+        if ($count == 0) return $this->result; 
+        $this->backtrackPermute($nums, 0, []);
+        return $this->result;
+    }
+
+    private function backtrackPermute($nums, $depth, $path)
+    {
+        if ($depth == count($nums)) {
+            $this->result[] = $path;
+            return;
+        }
+
+        for ($i = 0; $i < count($nums); $i++) {
+            if (in_array($nums[$i], $path)) continue;
+            $path[] = $nums[$i];
+            echo "before-i: ". $i. " ". $depth. " path: ". implode("-", $path). PHP_EOL;
+            $this->backtrackPermute($nums, $depth + 1, $path);
+            echo "after-i: ". $i. " ". $depth. " path: ". implode("-", $path). PHP_EOL;
+            // 回溯，恢复状态
+            array_pop($path);
+        }
+        echo "end-i: ". $i. " ". $depth. " path: ". implode("-", $path). PHP_EOL;
+    }
+}
+
+// $obj = new dfs();
+// $res = $obj->dfsMap();
+// print_r($res);
+
+$obj = new Solution();
+$res = $obj->permute([1, 2, 4]);
 print_r($res);
--- go/code/basic/MyInterface/main.go
+++ go/code/basic/MyInterface/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+    "database/sql"
     "fmt"
+    "sync"
     "test/extra"
     "test/worker"
+    "time"
 )
 
 func main()  {
@@ -17,6 +20,8 @@ func main()  {
 }
 
 func testCheckInterface()  {
+    // 用于触发编译期的接口的合理性检查机制
+    var _ extra.IAnimal = (*extra.Cat)(nil)
     //cat := new(extra.Cat)
     //cat.Name = "jimmy"
     //指针初始化
@@ -34,4 +39,17 @@ func testStaff()  {
     // 不能透过非公开类对成员赋值
     //staff.manager.Name = "Ben"
     fmt.Println(extra.Tell(staff))
+    //strconv.Atoi()
+    //sync.WaitGroup{}
+    var mu sync.Mutex
+    mu.Lock()
+    defer mu.Lock()
+    var rmu sync.RWMutex
+    var cpp = sync.Map{}
+    cpp.Store("name", "kk")
+    cpp.Range()
+    time.AfterFunc()
+    sync.Pool{New: func() interface{} {
+
+    }}
 }
