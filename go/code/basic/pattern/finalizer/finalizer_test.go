bff55a258887686f159ab67f2b4186ab7e7e9707
liangzhaocong <liangzhaocong@xinyu668.com>
2021-01-21 17:31:18 +0800

    feat: enhance finalizer sample

--- go/code/basic/pattern/finalizer/finalizer_test.go
+++ go/code/basic/pattern/finalizer/finalizer_test.go
@@ -21,3 +21,19 @@ func Test_FinalizerSample(t *testing.T) {
 	runtime.GC()
 	time.Sleep(1 * time.Second)
 }
+
+func Test_RoutineFinalizer(t *testing.T) {
+	wrapper := NewFinalizeObjWrapper()
+	for i := 0; i < 3; i++ {
+		wrapper.finalizeObj.rec <- fmt.Sprintf("data: abc-%v", i)
+	}
+	time.Sleep(500 * time.Millisecond)
+	fmt.Println("routine num: ", runtime.NumGoroutine())
+	// 不能确保每个变量都能触发回收
+	runtime.GC()
+	time.Sleep(1 * time.Second)
+	// 观察被回收的变量里的routine是否被关闭
+	fmt.Println("routine num: ", runtime.NumGoroutine())
+	// 变量对象仍有使用，不对变量进行垃圾回收
+	//wrapper.finalizeObj.rec <- "aa"
+}
--- go/code/basic/pattern/finalizer/routine_finalizer.go
+++ go/code/basic/pattern/finalizer/routine_finalizer.go
@@ -19,25 +19,29 @@ func NewFinalizeObjWrapper() *finalizeObjWrapper {
 		rec:   make(chan string),
 		close: make(chan struct{}),
 	}
+	// 开启协程异步处理数据
+	go obj.handle()
 	wrapper := &finalizeObjWrapper{obj}
 	// 只要协程一直在运行，Gc就无法选中对象执行垃圾回收。
-	//实际上，给ttlmap设置了SetFinalizer后，如果ttlmap.clear()协程没有return，而是阻塞了，Gc就可以将ttlmap回收掉。
+	// 实际上，给finalizeObj设置了SetFinalizer后，如果finalizeObj.handle()协程没有return，而是阻塞了，Gc就可以将finalizeObj回收掉。
 	runtime.SetFinalizer(wrapper, wrapperFinalizerHandle)
 	return wrapper
 }
 
-func (obj *finalizeObj) handle()  {
+func (obj *finalizeObj) handle() {
 	for {
 		select {
 		case <-obj.close:
 			fmt.Println("close finalize obj")
 			return
 		case data := <- obj.rec:
-			fmt.Println(data)
+			fmt.Println("rec data: ", data)
 		}
 	}
 }
 
 func wrapperFinalizerHandle(wrapper *finalizeObjWrapper) {
+	fmt.Println("finalizer working")
+	// 变量进行垃圾回收触发关闭routine 避免变量被回收，里面的协程未关闭出现内存泄露
 	wrapper.finalizeObj.close <- struct{}{}
 }
