cbc1cbab39d26d744127bb3704e9f7215e052356
byron <liangzhaocong@163.com>
2021-08-27 11:57:47 +0800

    feat: add algo for bit search

--- go/code/basic/alog/search_test.go
+++ go/code/basic/alog/search_test.go
@@ -1,10 +1,17 @@
 package alog
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestMinArray(t *testing.T) {
-    res := MinArray([]int{4,5,5,6,1,2,3,4})
-    if res != 1 {
-        t.Error("Not min data")
-    }
+	res := MinArray([]int{4, 5, 5, 6, 1, 2, 3, 4})
+	if res != 1 {
+		t.Error("Not min data")
+	}
+}
+
+func TestBitSearch(t *testing.T) {
+	res := BitSearch(-10, 3)
+	t.Log(res)
 }
